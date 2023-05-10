3a21bf682a474efb72254fd24554cb3c5ac4140e
grigoriy <gz@gamil.com>
2023-05-10 16:40:59 +0300

    add solution with code

--- solution/Program.cs
+++ solution/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] InputArray(string[] arr)
+{
+    Console.WriteLine("Enter elements of array via enter button");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Convert.ToString(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} \t");
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Enter number of array elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+InputArray(array);
+Console.WriteLine("Input array:");
+PrintArray(array);
+
+int m = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    string b = array[i];
+    int a = b.Length;
+    if (a <= 3)
+    {
+        m++;
+    }
+}
+string[] newArray = new string[m];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    string b = array[i];
+    int a = b.Length;
+    if (a <= 3)
+    {
+        newArray[j] = array[i];
+        j++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Result:");
+PrintArray(newArray);
